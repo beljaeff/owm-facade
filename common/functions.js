edb128034e041acdcb373ee6fe1ae17a69345893
Yuri D. Beljaeff <beljaeff@gmail.com>
2021-08-26 18:48:29 +0300

    getBoolean* refactored

--- common/functions.js
+++ common/functions.js
@@ -1,16 +1,26 @@
-function getBooleanValue(stringValue) {
-    stringValue = String(stringValue).toLowerCase();
-    if (stringValue === 'true' || stringValue === 'yes' || stringValue === 'y') {
+function getBooleanValue(value) {
+    const type = typeof value;
+    if (type !== 'string') {
+        throw new Error(`String expected, but '${value}' is '${type}'`);
+    }
+
+    value = value.toLowerCase();
+    if (value === 'true' || value === 'yes' || value === 'y') {
         return true;
     }
-    else if (stringValue === 'false' || stringValue === 'no' || stringValue === 'n') {
+    else if (value === 'false' || value === 'no' || value === 'n') {
         return false;
     }
-    throw new Error(`Can't understand '${stringValue}', should be one of true|yes|y|false|no|n`);
+
+    throw new Error(`Can't understand '${value}', should be one of true|yes|y|false|no|n`);
 }
 
 export function getBooleanAppParam(envValue, configValue, defaultValue = false) {
-    return typeof envValue    !== 'undefined' ? getBooleanValue(envValue)
-         : typeof configValue !== 'undefined' ? getBooleanValue(configValue)
-         : defaultValue;
+    if (typeof envValue !== 'undefined') {
+        return getBooleanValue(envValue);
+    }
+    else if (typeof configValue !== 'undefined') {
+        return getBooleanValue(configValue)
+    }
+    return defaultValue;
 }
